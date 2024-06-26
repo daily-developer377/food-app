b721a48dc4106f0ddd2c90e17bbd76d91458a773
nandhuaries <nandhu.s@ariesesolutions.com>
2024-06-26 09:59:47 +0530

    completed Hooked video

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -6,30 +6,25 @@ import mockData from "../utils/mockData";
 import { useState } from "react";
 
 const Body = () => {
-  const [listOfRestaurantss, setListOfRestaurantss] = useState([
-    {
-      id: "74453",
-      name: "Domino's Pizza",
-      avgRating: "4.0",
-      cloudinaryImageId: "bz9zkh2aqywjhpankb07",
-      cuisines: ["Pizzas"],
-    },
-    {
-      id: "74454",
-      name: "KFc",
-      avgRating: "4.0",
-      cloudinaryImageId: "bz9zkh2aqywjhpankb07",
-      cuisines: ["Pizzas"],
-    },
-  ]);
+  const [listOfRestaurantss, setListOfRestaurantss] = useState(mockData);
 
   return (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn">Top Rated Restaurants</button>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurantss.filter(
+              (res) => res.data.avgRating > 4.2
+            );
+            setListOfRestaurantss(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
       </div>
       <div className="res-container">
-        {mockData.map((restaurant) => {
+        {listOfRestaurantss.map((restaurant) => {
           return <RestroCards {...restaurant.data} key={restaurant.data.id} />;
         })}
       </div>
