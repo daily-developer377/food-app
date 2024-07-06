d63a546f8d4d60de57db1a08131a370a857af7b9
nandhuaries <nandhu.s@ariesesolutions.com>
2024-07-06 16:12:13 +0530

    fixed the swiggy api issue

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,69 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../App.css";
 import RestroCards from "./RestroCards";
-// import restaurantList from "./RestaurantList";
-import mockData from "../utils/mockData";
 import SearchBar from "./SearchBar";
-import { useState } from "react";
-import { useEffect } from "react";
 
 const Body = () => {
-  const [listOfRestaurantss, setListOfRestaurantss] = useState([]);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
   useEffect(() => {
-    console.log("hii use effect called  ");
     fetchData();
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    const response = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=10.1334634&lng=76.3976648&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      // "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
     );
 
-    const json = await data.json();
-    console.log(json);
-
-    // Optional Chaining
-    setListOfRestaurantss(
-      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    const json = await response.json();
+    setListOfRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants || []
     );
 
-    // console.log(
-    //   json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
-    //     ?.restaurants[2].info.name
-    // );
+    console.log("huu", json);
+  };
+
+  const filterTopRated = () => {
+    const filteredList = listOfRestaurants.filter(
+      (res) => res.info.avgRating > 4.2
+    );
+    setListOfRestaurants(filteredList);
   };
 
   return (
     <div className="body">
       <div className="filter">
-        <button
-          className="filter-btn"
-          onClick={() => {
-            const filteredList = listOfRestaurantss.filter(
-              (res) => res.data.avgRating > 4.2
-            );
-            setListOfRestaurantss(filteredList);
-          }}
-        >
+        <button className="filter-btn" onClick={filterTopRated}>
           Top Rated Restaurants
         </button>
         <SearchBar />
       </div>
       <div className="res-container">
-        {listOfRestaurantss.map((restaurant) => {
-          return (
-            <RestroCards
-              resData={restaurant?.info}
-              key={restaurant?.info?.id || restaurant.data.id}
-            />
-          );
-        })}
+        {listOfRestaurants.map((restaurant) => (
+          <RestroCards
+            key={restaurant.info.id}
+            name={restaurant.info.name}
+            cuisines={restaurant.info.cuisines}
+            avgRating={restaurant.info.avgRating}
+            cloudinaryImageId={restaurant.info.cloudinaryImageId}
+          />
+        ))}
       </div>
-
-      {/* <RestroCards {...restaurantList[0].data} />
-      <RestroCards {...restaurantList[0].data} />
-      <RestroCards {...restaurantList[0].data} /> */}
     </div>
   );
 };
