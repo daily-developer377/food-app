d954631caa915373c18503f5334ec8aeec7a2f9c
nandhuaries <nandhu.s@ariesesolutions.com>
2024-07-14 17:52:54 +0530

    added CROS PROXY in the swiggy url

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "../App.css";
 import RestroCards from "./RestroCards";
-import SearchBar from "./SearchBar";
 import ShimmerUi from "./Shimmer";
 
 const Body = () => {
@@ -17,7 +16,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const response = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=10.1334634&lng=76.3976648&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=10.1334634&lng=76.3976648&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
       // "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
     );
 
